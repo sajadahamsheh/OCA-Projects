a5e2325df20d8bf8d855ec8ea362a572943f2d8c
sajaDahamsheh <saja.dahamsheh97@gmail.com>
2021-02-06 22:01:37 +0200

    project6

---
+++ project6-E-Commerce-Solution-PHP-and-MYSQL/admin/includes/connection.php
@@ -0,0 +1,8 @@
+<?php
+// open connection
+$conn = mysqli_connect("localhost", "root", "", "project6");
+if (!$conn) {
+    die('cannot connecto to server');
+}
+
+
---
+++ project6-E-Commerce-Solution-PHP-and-MYSQL/public/includes/db_connection.php
@@ -0,0 +1,6 @@
+<?php
+    $conn =mysqli_connect("localhost","root","","project6");
+    if(!$conn){
+        die("cannot connect to server");
+    }
+?>
